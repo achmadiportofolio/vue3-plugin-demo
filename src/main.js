119057d3c671fcb2c35a8887bd92f37a3675d483
achmadi-asus <diahmad330@gmail.com>
2021-09-30 06:08:49 +0700

    add support taiwind

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import MyFirstPlugin from './plugins/MyFirstPlugin'
-
+import './assets/tailwind.css'
 const app = createApp(App)
 app.use(MyFirstPlugin, {fontSize: {
     small: 20,
